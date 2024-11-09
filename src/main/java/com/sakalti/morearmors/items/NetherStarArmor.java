8b5307c4ef99369950aaa2c30ea63045cfffc6b3
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-11-09 16:26:35 +0900

    Update NetherStarArmor.java

--- src/main/java/com/sakalti/morearmors/items/NetherStarArmor.java
+++ src/main/java/com/sakalti/morearmors/items/NetherStarArmor.java
@@ -2,7 +2,10 @@ package com.sakalti.morearmors.items;
 
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
 
 public class NetherStarArmor extends ArmorItem {
     public NetherStarArmor(EquipmentSlot slot, Properties properties) {
@@ -10,7 +13,18 @@ public class NetherStarArmor extends ArmorItem {
     }
 
     @Override
+    public boolean isEnchantable(ItemStack stack) {
+        return true;  // エンチャント可能にする
+    }
+
+    @Override
     public String getArmorTexture(ItemStack stack, EquipmentSlot slot, String type) {
         return slot == EquipmentSlot.LEGS ? "morearmors:textures/models/armor/nether_star_armor_2.png" : "morearmors:textures/models/armor/nether_star_armor_1.png";
+    }
+
+    // クリエイティブタブを指定
+    @Override
+    public CreativeModeTabs getCreativeModeTabs() {
+        return CreativeModeTabs.COMBAT;
     }
 }
